b8e17e0ee02b4007086c8745c7de5fac998013e9
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-07-23 16:21:25 +0900

    ✨ Feat:"인증 미들웨어 구현"

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import UsersRouter from "./route/user.router";
+
+const app = express();
+const PORT = 3018;
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api", [UsersRouter]);
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
