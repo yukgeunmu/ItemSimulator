597fdab3be017dc0a16e5678a2e35f3ecb32f63f
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-07-24 15:37:10 +0900

    ✨ Feat:"로그인 API 구현"

--- src/route/user.router.js
+++ src/route/user.router.js
@@ -59,10 +59,33 @@ router.post('/sign-up', async (req, res, next) => {
       },
     );
 
-    return res.status(200).json({message:"회원가입이 완료되었습니다."});
+    return res.status(200).json({ message: '회원가입이 완료되었습니다.' });
   } catch (err) {
     next(err);
   }
+});
+
+router.post('/sign-in', async (req, res, next) => {
+  const { userId, password } = req.body;
+
+  const user = await prisma.account.findFirst({ where: { userId } });
+
+  if (!user) return res.status(409).json({ message: '존재하지 않는 이메일 입니다.' });
+
+  if (!(await bcrpyt.compare(password, user.password)))
+    return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    'custom-secret-key',
+  );
+
+  res.cookie("authorization", `Bearer ${token}`);
+
+   return res.status(200).json({ message: "로그인 성공" });
+
 });
 
 export default router;
